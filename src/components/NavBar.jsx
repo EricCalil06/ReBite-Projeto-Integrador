5bf45a380f33ef5deb2c564d9be4dfd38f9058d7
Eric Calil Souto <eric.souto06@gmail.com>
2026-05-04 22:18:48 -0300

    feat: Adiciona a logo para a navbar e reorganiza o visual dos botões

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,30 +1,46 @@
 import { Link } from "react-router-dom";
+import logoReBiteH from "../assets/logoReBiteH.png";
 
 function NavBar({ user }) {
   return (
-    <nav className="flex justify-between p-4 bg-slate-800 text-white">
-      {/* LADO ESQUERDO DA NAVBAR */}
-      <div className="flex gap-4 items-center mr-4 ml-4">    
+    <nav className="flex justify-between p-4 text-black bg-[#FFFBF9] mr-60 ml-60 mt-4 rounded-xl">
+
+      {/* Itens a esquerda da NavBar */}
+      <div className="flex gap-4 items-center mr-4 ml-4 ">
+        <Link to="/">
+          <img
+            src={logoReBiteH}
+            alt="Logo ReBite"
+            className="w-32 mb-4 mr-4 items-center justify-center mx-auto"
+          />
+        </Link>
         <Link to="/">Início</Link>
         <Link to="/sobre">Sobre</Link>
-      </div>    
-      
-      {/* LADO DIREITO DA NAVBAR */}
-      <div className="flex gap-4 items-center mr-4 ml-4">    
+      </div>
+
+      {/* Itens a direita da NavBar */}
+      <div className="flex gap-4 items-center mr-4 ml-4">
         {!user ? (
           <Link to="/login">
-             <button className="bg-blue-600 p-2 rounded">Fazer Login</button>
+            <button className="bg-[#F55D22] p-3 text-white font-bold w-[120px] rounded-xl">
+              Fazer Login
+            </button>
           </Link>
         ) : (
           <>
-            <h1>Bem Vindo {user.name}!</h1>
-            <button>Meu Perfil</button>
-            
+            <button className="bg-[#F55D22] p-3 text-white font-bold w-[120px] rounded-xl">
+              Meu Perfil
+            </button>
+
             {user.hasEstablishment && (
-              <button className="bg-green-600 p-2 rounded">Acessar Loja</button>
+              <button className="bg-green-600 p-3 text-white font-semibold w-[120px] rounded-xl">
+                Acessar Loja
+              </button>
             )}
 
-            <button className="bg-red-600 p-2 rounded">Sair</button>
+            <button className="bg-red-600 p-3 text-white font-semibold w-[120px] rounded-xl">
+              Sair
+            </button>
           </>
         )}
       </div>
