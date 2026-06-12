16a26e41e8ea96db46b26a6ad260c6d3cbbd4c85
Eric C Souto <eric.souto06@gmail.com>
2026-06-12 15:36:14 -0300

    fix: arruma scroll lateral de todas as telas

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <BrowserRouter>
-      <div className="w-screen h-screen bg-slate-50">
+      <div className="w-full min-h-screen bg-slate-50">
         <NavBar user={user} />
 
         <Routes>
